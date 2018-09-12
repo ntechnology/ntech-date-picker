4b71e937816b37e0863ea833a28887643843b9cc
ntechnology <43126134+ntechnology@users.noreply.github.com>
2018-09-12 20:27:27 +0630

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<Head>
+    <link href="jquery-ui-1.11.4/smoothness/jquery-ui.css" rel="stylesheet" />
+    <script src="jquery-ui-1.11.4/external/jquery/jquery.js"></script>
+    <script src="jquery-ui-1.11.4/jquery-ui.js"></script>
+    <script src="jquery-ui-1.11.4/jquery-ui.min.js"></script>
+    <link rel="stylesheet" href="jquery-ui-1.11.4/jquery-ui.theme.css">
+    <script>
+      $(document).ready(function(){
+	       $("#tanggal").datepicker({
+	        })
+        })
+    </script>
+</head>
+<body>
+   Tanggal : <input type="text" name="tanggal" id="tanggal" />
+ </body>
+</html>
